08cdec6af814ab66458b22b17436c0b8160245bd
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-01-07 17:31:54 +0000

    Solve exercise_2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id: 00)
+@store1.annual_revenue = 290000
+@store1.save
+
+@store2 = Store.find_by(id:01)
+@store2.annual_revenue = 2000000
+@store2.save
